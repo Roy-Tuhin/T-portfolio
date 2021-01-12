8062e480f0f764028ee481b128e2e5cc77dc790d
Boyetuhin <tuhinroy881@gmail.comgit config --global user.name>
2021-01-13 01:35:13 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+/* ---------------------------------------Navigation menu--------------------------------------------------*/
+// (() =>{
+
+//      const hamburgerBtn = document.querySelector(".hamburger-btn"),
+//      navMenu = document.querySelector(".nav-Menu"),
+//      closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+//      hamburgerBtn.addEventListener("click", showNavMenu);
+//      closeNavBtn.addEventListener("click", hideNavMenu);
+
+
+//      function showNavMenu(){
+//           navMenu.classList.add("open");
+//      }
+//      function hideNavMenu(){
+//           navMenu.classList.remove("open");
+//      }
+// })
+
+
+/* ---------------------------------------About section tabs--------------------------------------------------*/
+(() =>{
+     const aboutSection = document.querySelector(".about-section"),
+     tabsContainer = document.querySelector(".about-tabs");
+
+     tabsContainer.addEventListener("click", (event) =>{
+          /* if event.target contains 'tab-item' class and not contains 'active' class*/
+          if(event.target.classList.contains("tab-item") &&
+          !event.target.classList.contains("active")){
+               const target = event.target.getAttribute("data-target");
+               //deactivate existing active 'tab-item'
+               tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
+               //active new 'tab-item'
+               event.target.classList.add("active","outer-shadow");
+
+
+               //deactivate existing active 'tab-content'
+               aboutSection.querySelector(".tab-content.active").classList.remove("active");
+               //active new 'tab-content'
+               aboutSection.querySelector(target).classList.add("active");
+          }
+     })
+})();
+
+
+
+
+
+
+/* ---------------------------------------hide all section except active-----------------------------------------*/
+
+// (()=>{
+
+//      const section = document.querySelectorAll(".section");
+//      sections.forEach((section) =>{
+//           if(!section.classList.contains("active")){
+//                section.classList.add("hide");
+//           }
+//      })
+// })();
