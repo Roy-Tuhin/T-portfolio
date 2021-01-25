0c65953557dcf1c121bb4dbf6f0514e271cc9a45
Boyetuhin <tuhinroy881@gmail.comgit config --global user.name>
2021-01-26 00:21:40 +0530

    clean look

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@
 //      hamburgerBtn.addEventListener("click", showNavMenu);
 //      closeNavBtn.addEventListener("click", hideNavMenu);
 
-
 //      function showNavMenu(){
 //           navMenu.classList.add("open");
 //      }
@@ -17,35 +16,34 @@
 //      }
 // })
 
-
 /* ---------------------------------------About section tabs--------------------------------------------------*/
-(() =>{
-     const aboutSection = document.querySelector(".about-section"),
-     tabsContainer = document.querySelector(".about-tabs");
+(() => {
+  const aboutSection = document.querySelector(".about-section"),
+    tabsContainer = document.querySelector(".about-tabs");
 
-     tabsContainer.addEventListener("click", (event) =>{
-          /* if event.target contains 'tab-item' class and not contains 'active' class*/
-          if(event.target.classList.contains("tab-item") &&
-          !event.target.classList.contains("active")){
-               const target = event.target.getAttribute("data-target");
-               //deactivate existing active 'tab-item'
-               tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
-               //active new 'tab-item'
-               event.target.classList.add("active","outer-shadow");
+  tabsContainer.addEventListener("click", (event) => {
+    /* if event.target contains 'tab-item' class and not contains 'active' class*/
+    if (
+      event.target.classList.contains("tab-item") &&
+      !event.target.classList.contains("active")
+    ) {
+      const target = event.target.getAttribute("data-target");
+      //deactivate existing active 'tab-item'
+      tabsContainer
+        .querySelector(".active")
+        .classList.remove("outer-shadow", "active");
+      //active new 'tab-item'
+      event.target.classList.add("active", "outer-shadow");
 
-
-               //deactivate existing active 'tab-content'
-               aboutSection.querySelector(".tab-content.active").classList.remove("active");
-               //active new 'tab-content'
-               aboutSection.querySelector(target).classList.add("active");
-          }
-     });
+      //deactivate existing active 'tab-content'
+      aboutSection
+        .querySelector(".tab-content.active")
+        .classList.remove("active");
+      //active new 'tab-content'
+      aboutSection.querySelector(target).classList.add("active");
+    }
+  });
 })();
-
-
-
-
-
 
 /* ---------------------------------------hide all section except active-----------------------------------------*/
 
